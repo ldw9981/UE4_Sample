8226f7995372d379a8e953b66b2db6bdaf696c31
LeeDongWon <ldw9981@gmail.com>
2022-01-29 21:08:05 +0900

    spawner

---
+++ FPSCPP/Source/FPSCPP/ItemPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "ItemPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSCPP_API AItemPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
---
+++ FPSCPP/Source/FPSCPP/MonsterPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "MonsterPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSCPP_API AMonsterPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
